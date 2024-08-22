6480e2af3f63accf0a7b9d40c88bdeaf6a8ef259
Dmitriy <dmytro.marynenko@scive.ai>
2024-08-22 15:50:17 +0300

    Add components

---
+++ src/shared/components/ui/index.ts
@@ -0,0 +1,6 @@
+export * from './Drawer';
+export * from './Dialog';
+export * from './Popover';
+export { Button } from './Button';
+export { Checkbox } from './Checkbox';
+export { Input } from './Input';
