6119084887da244d19a1d56c559759b8ed069599
Dmitriy <dmytro.marynenko@scive.ai>
2024-08-23 23:22:13 +0300

    Fix range slider count

--- src/shared/components/shared/RangeSlider/RangeSlider.tsx
+++ src/shared/components/shared/RangeSlider/RangeSlider.tsx
@@ -49,17 +49,17 @@ const RangeSlider = React.forwardRef(
                 <SliderPrimitive.Track className='relative h-1 w-full grow overflow-hidden rounded-full bg-primary/20'>
                     <SliderPrimitive.Range className='absolute h-full bg-primary' />
                 </SliderPrimitive.Track>
-                {localValues.map((localValue, index) => (
+                {localValues.map((item, index) => (
                     <React.Fragment key={index}>
                         <div
                             className='absolute text-center'
                             style={{
-                                left: `calc(${((localValue - min) / (max - min)) * 100}% + 0px)`,
+                                left: `calc(${((item - min) / (max - min)) * 100}% + 0px)`,
                                 top: '10px',
                             }}
                         >
                             <span className='text-sm'>
-                                {formatLabel ? formatLabel(localValue) : value}
+                                {formatLabel ? formatLabel(item) : item}
                             </span>
                         </div>
                         <SliderPrimitive.Thumb className='block h-4 w-4 rounded-full border border-primary/50 bg-white shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50' />
